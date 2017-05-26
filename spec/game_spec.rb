9fa391f8ef9ddf3b81a0b02792773ecba6120cef
Panteha <panteha.ahmadi@gmail.com>
2017-05-26 10:08:15 +0100

    Class extraction to the game class

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,13 +2,14 @@ require 'game'
 
 describe Game do
 
+  subject(:game) {described_class.new}
   let(:jade) { double('Jade', attack:'' ) }
   let(:lubos) { double('Lubos', get_damaged:'') }
 
   describe '#attack' do
     it "attacks the opponent" do
       expect(lubos).to receive(:get_damaged)
-      jade.attack(lubos)
+      game.attack(lubos)
     end
   end
 
