6c7a8f876cbf8271019bc44b04dd45ac5a549484
jade <jadealvares@hotmail.co.uk>
2017-05-26 12:10:30 +0100

    implement functionality to switch turns

--- spec/features/switch_turns_spec.rb
+++ spec/features/switch_turns_spec.rb
@@ -4,3 +4,18 @@ feature 'Player 1 attacks player 2' do
     expect($game.current_game.name).to eq 'Jade'
   end
 end
+
+feature 'to see current turn' do
+  scenario 'player 1 turn' do
+    sign_in_and_play
+    expect(page).to have_content "Jade's turn"
+  end
+end
+
+feature 'after first attack, the turn should switch' do
+  scenario "player 2 turn" do
+    sign_in_and_play
+    expect(page).not_to have_content "Jade's turn"
+    expect(page).to have_content "Lubos's turn"
+  end
+end
